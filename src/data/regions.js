e152267c27cc76349a9a1adc49f97a6a1d79f767
thiran1 <51041128+thiran1@users.noreply.github.com>
2026-06-28 18:13:33 +0530

    Create regions.js

---
+++ src/data/regions.js
@@ -0,0 +1,36 @@
+const regions = [
+  {
+    id: 1,
+    state: "Telangana",
+    slug: "telangana",
+    heroImage: "/images/regions/telangana.jpg",
+    description:
+      "Home to nutritious millet-based beverages that have fueled generations.",
+    drinkCount: 1,
+    drinks: ["ragi-ambali"]
+  },
+
+  {
+    id: 2,
+    state: "Tamil Nadu",
+    slug: "tamil-nadu",
+    heroImage: "/images/regions/tamilnadu.jpg",
+    description:
+      "A rich culinary tradition with refreshing drinks crafted for tropical summers.",
+    drinkCount: 1,
+    drinks: ["jigarthanda"]
+  },
+
+  {
+    id: 3,
+    state: "Maharashtra",
+    slug: "maharashtra",
+    heroImage: "/images/regions/maharashtra.jpg",
+    description:
+      "The Konkan coast brings naturally cooling fruit-based beverages.",
+    drinkCount: 1,
+    drinks: ["kokum-sharbat"]
+  }
+];
+
+export default regions;
